9d6310179cddac58179cfec5cfcbfb6c08b59243
Shakyan Kushwaha <32445019+anandamideShakyan@users.noreply.github.com>
2022-12-29 22:01:13 +0530

    Update PostRequest.js

--- src/api/PostRequest.js
+++ src/api/PostRequest.js
@@ -1,7 +1,5 @@
 import axios from "axios"
-
-// const API = axios.create({ baseURL: "http://localhost:5001" })
-
-export const getTimelinePosts = (id) => axios.get(`/post/${id}/timeline`, id)
+const API = axios.create({ baseURL: `${process.env.REACT_APP_API_URL}` })
+export const getTimelinePosts = (id) => API.get(`/post/${id}/timeline`, id)
 export const likePost = (id, user_id) =>
-	axios.post(`/post/${id}/like`, { userId: user_id })
+	API.post(`/post/${id}/like`, { userId: user_id })
