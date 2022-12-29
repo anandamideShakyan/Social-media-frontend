9eb14b9f2fb77f1c7c2ed005f242307b5c08448d
Shakyan Kushwaha <32445019+anandamideShakyan@users.noreply.github.com>
2022-12-29 21:59:42 +0530

    Update AuthRequest.js

--- src/api/AuthRequest.js
+++ src/api/AuthRequest.js
@@ -1,9 +1,9 @@
 import axios from "axios"
 
-// const API = axios.create({ baseURL: "http://localhost:5001" })
+const API = axios.create({ baseURL: `${process.env.REACT_APP_API_URL}` })
 
-export const logIn = (formData) => axios.post("/auth/login", formData)
-export const signUp = (formData) => axios.post("/auth/register", formData)
+export const logIn = (formData) => API.post("/auth/login", formData)
+export const signUp = (formData) => API.post("/auth/register", formData)
 export const clearCookie = () =>
 	axios.post(
 		"/logout",
